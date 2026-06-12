4e62b75a951136c3c09e8b7dcad583e2cb873471
abhishek1165 <abhishekkachha12345@gmail.com>
2026-06-12 17:33:15 +0530

    Add new WEBP images for VSCode and windsurf assets
    - Introduced a new image file `vscode.webp` to enhance the visual assets for the project.
    - Added `windsurf.webp` image to further diversify the asset library.

---
+++ app/components/footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <main>Footer</main>
+  )
+}
+
+export default Footer
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Sidebar from "./components/sidebar";
+import Footer from "./components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +25,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en">
+      <body className={`${geistSans.className}${geistMono.className} bg-zinc-950 text-zinc-100 antialiased`}>
+        <div className="min-h-screen flex">
+          <Sidebar />
+          <div className= " flex-1 md:ml-65 ml-10px">
+            <main className="min-h-screen">{children}</main>
+             <Footer/>
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
