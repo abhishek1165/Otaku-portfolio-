38527f9fad98ea416073c3836d13dcd3aa945376
abhishek1165 <abhishekkachha12345@gmail.com>
2026-06-20 22:43:34 +0530

    feat: implement ProjectsPage component and add projects data structure; enhance project management capabilities

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,28 @@
+"use client";
 import React from 'react'
 
-const Projects = () => {
+import {useRouter} from 'next/navigation';
+import {projectStyles as s} from '@/public/dummyStyles'; 
+import {projects,Project} from "@/app/lib/projects-data"
+
+const ProjectsPage = () => {
   return (
-    <div>Projects</div>
+    <div className={s.pageContainer}>
+      <div className={s.innerContainer}>
+        <div className={s.header}>
+          <h1 className={s.pageTitle}>
+            Projects
+          </h1>
+            <p className={s.pageSubtitle}>
+               Playground - Small MVP to Production Apps
+
+            </p>
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
-export default Projects
+export default ProjectsPage
