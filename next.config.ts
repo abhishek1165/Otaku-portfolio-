39709de3b0c9227624e601759c7a21816b8efa81
abhishek1165 <abhishekkachha12345@gmail.com>
2026-07-24 12:05:01 +0530

    feat: Update next.config.ts with output settings and image optimization

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",          
+  trailingSlash: true,       
+  images: {
+    unoptimized: true,       
+  },
   allowedDevOrigins: ["fawn-piazzaed-cleveland.ngrok-free.dev"],
 };
 
